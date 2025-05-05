1f55aea075b05db3af1bd366210253c27faa5112
Anne Marte Hjemås <kons-ahj@digdir.no>
2025-05-05 12:52:30 +0200

    ID-5340: oppdatere til java 17 og eidas 2.9.0 (#31)
    * oppgradere til java 17 og eidas 2.9
    
    * lisens
    
    * workflow
    
    * uteglemmelser i docker fil

--- docker/connector/tomcat-setenv.sh
+++ docker/connector/tomcat-setenv.sh
@@ -1,7 +1,7 @@
 
 # bouncycastle. 
 export JAVA_OPTS="$JAVA_OPTS -Djava.security.properties=/opt/java/openjdk/conf/security/java_bc.security"
-export JAVA_OPTS="$JAVA_OPTS --module-path /usr/local/lib/bcprov-jdk18on-1.78.jar"
+export JAVA_OPTS="$JAVA_OPTS --module-path /usr/local/lib/bcprov-jdk18on-1.78.1.jar"
 export JAVA_OPTS="$JAVA_OPTS --add-modules org.bouncycastle.provider"
 
 # eidas config
